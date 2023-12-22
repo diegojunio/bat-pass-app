529601142f38126b794a5f3035beeb44d953c928
diegojunio <diegojunio@gmail.com>
2023-12-22 13:30:57 -0300

    first commit

---
+++ src/components/BatButton/BatButton.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Text, Pressable } from 'react-native';
+import { styles } from './BatButtonStyles';
+import { BatTextInput } from '../BatTextInput/BatTextInput';
+import generatePass from '../../services/passwordService';
+import * as Clipboard from 'expo-clipboard';
+
+export function BatButton() {
+    const [pass, setPass] = useState('');
+
+    function handleGenerateButton(){
+        let generateToken = generatePass();
+        setPass(generateToken);
+    }
+
+    function renderCopyButton(){
+        console.log(pass);
+        Clipboard.setStringAsync(pass);
+    }
+    
+    return (
+    <>
+        <BatTextInput pass={pass}/>
+        <Pressable
+            style={ styles.button }
+            onPress={ handleGenerateButton }
+        >
+            <Text style={ styles.text }>
+                GENERATE
+            </Text>
+        </Pressable>
+        
+        <Pressable
+            style={ styles.button }
+            onPress={ renderCopyButton }
+        >
+            <Text style={ styles.text }>
+                COPY
+            </Text>
+        </Pressable>
+    </>
+  );
+}
